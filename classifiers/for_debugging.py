32f186ae4f7ddd5c5ef1a94097372a759b240e29
Yanyu Liang <yanyul@andrew.cmu.edu>
2016-04-10 20:52:48 -0400

    Add motif fueature and structure feature demos

--- classifiers/for_debugging.py
+++ classifiers/for_debugging.py
@@ -2,28 +2,6 @@
 import sys
 import lib_classifier
 
-def gc_content(seq, param):
-		tmp = list(seq)
-		length = len(seq)
-		parts_num = param
-		part_size = int(length / param)
-		part_starts = [0]
-		
-		for i in range(1, parts_num):
-			part_starts.append(i * part_size)
-		
-		re = []
-		
-		for part in part_starts:
-			subseq = seq[part : part + part_size]
-			tmp_count = 0
-			for c in subseq:
-				if c == 'G' or c == 'C':
-					tmp_count += 1
-			re.append(float(tmp_count) / part_size)
-
-		return re
-
 def data_read(mf):
 	raw_data = []
 	labels = []
@@ -52,19 +30,32 @@ my_in = sys.argv[1] # training set
 my_test = sys.argv[2] # test set
 
 (raw_data, labels) = data_read(my_in) # read training set from file
-
+#print(len(raw_data))
 feature_gen = lib_classifier.Feature_Generator() # create feature generator
-feature_gen.add_function('gc_content', 5) # define first feature generation method (here is trivil -- GC content)
-feature_gen.add_function('motif_score', 'meme_probMatrix.txt') # define another feature generation method (here is trivil -- GC content)
+feature_gen.add_function('gc_content', 10) # usage of GC content feature. NUM means how many bins 
+feature_gen.add_function('motif_score', ['meme_probMatrix.txt', '-b']) # usage of motif score feature. 
+feature_gen.add_function('motif_score', ['motif1.motif', '-b']) # param is [motif_file_name, mode], where if mode == '-b', it will only return best score
+feature_gen.add_function('motif_score', ['motif2.motif', '-b']) 
+feature_gen.add_function('motif_score', ['motif3.motif','-b']) 
+feature_gen.add_function('motif_score', ['motif4.motif', '']) 
+feature_gen.add_function('rna_struct', 10) # usage of RNA secondary structure feature. NUM means the length of the sequence you want to analyze the structure. See the following details
 
-my_svm = lib_classifier.My_Classifier(feature_gen, 'svm', [10,1,'rbf']) # create a classifier 
+
+
+## Suppos here is a sequence (we assume that the very middle one is the TTS) 
+## ---------------UP-------MID-------DOWN------------
+## rna_struct will return secondary structure of UP------MID, MID------DOWN, and UP----------------DOWN
+## where ( is labeled as -1, . is labeled as 0 and ) is labeled as +1
+## example: (((.....)))  ==> [-1,-1,-1,0,0,0,0,0,1,1,1]
+
+my_classifier = lib_classifier.My_Classifier(feature_gen, 'svm', [100,1,'linear']) # create a classifier 
 									# 1: feature generator defined above ; 2: classifier type ; 3. param for classifier
 									# now we only have 'svm' and 'logistic' 
-my_svm.set_up_param() # set param for classifer (sorry, now this is not automatically, so we need to run this line)
+my_classifier.set_up_param() # set param for classifer (sorry, now this is not automatically, so we need to run this line)
 
-my_svm.train(raw_data, labels) # training
+my_classifier.train(raw_data, labels) # training
 (raw_data_test, labels_test) = data_read(my_test) # read test set from file
-re = my_svm.predict(raw_data_test) # predict
+re = my_classifier.predict(raw_data_test) # predict
 accuracy(re, labels_test) # generate a report for prediction
-my_svm.visualize(raw_data, labels) # visualize data as scatter polt (PCA is applied)
+#my_classifier.visualize(raw_data, labels) # visualize data as scatter polt (PCA is applied)
 
