a60549e9e4cd1915fea9feba62186f4344f77573
Yanyu Liang <yanyul@andrew.cmu.edu>
2016-04-17 15:38:21 -0400

    Test new kernel rbf_linear_hamming

--- classifiers/for_debugging.py
+++ classifiers/for_debugging.py
@@ -32,7 +32,7 @@ my_test = sys.argv[2] # test set
 (raw_data, labels) = data_read(my_in) # read training set from file
 #print(len(raw_data))
 feature_gen = lib_classifier.Feature_Generator() # create feature generator
-feature_gen.add_function('gc_content', 20) # usage of GC content feature. NUM means how many bins 
+feature_gen.add_function('gc_content', 10) # usage of GC content feature. NUM means how many bins 
 feature_gen.add_function('motif_score', ['meme_probMatrix.txt', '-b']) # usage of motif score feature. 
 feature_gen.add_function('motif_score', ['motif1.motif', '-b']) # param is [motif_file_name, mode], where if mode == '-b', it will only return best score
 feature_gen.add_function('motif_score', ['motif2.motif', '-b']) 
@@ -41,10 +41,10 @@ feature_gen.add_function('motif_score', ['motif4.motif', '-b'])
 feature_gen.add_function('rna_struct', [7, '-up']) # usage of RNA secondary structure feature. NUM means the length of the sequence you want to analyze the structure. more details are in lib_classifier
 
 
-my_classifier = lib_classifier.My_Classifier(feature_gen, 'tree', [100,1,'sigmoid']) # create a classifier 
+my_classifier = lib_classifier.My_Classifier(feature_gen, 'svm', [10,1,'rbf_linear_hammington', 1]) # create a classifier 
 									# 1: feature generator defined above ; 2: classifier type ; 3. param for classifier
 									# now we only have 'svm' and 'logistic' 
-my_classifier.set_up_param() # set param for classifer (sorry, now this is not automatically, so we need to run this line)
+#my_classifier.set_up_param() # set param for classifer (sorry, now this is not automatically, so we need to run this line)
 
 my_classifier.train(raw_data, labels) # training
 (raw_data_test, labels_test) = data_read(my_test) # read test set from file
