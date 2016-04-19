ebd3795bd77585432e9c569093bb685cd0124501
Yanyu Liang <yanyul@andrew.cmu.edu>
2016-04-18 21:52:53 -0400

    this is the main function of tts_prediction software, which takes one pretrained classifier (use tts_prediction_train.py) and a genome FASTA file

---
+++ classifiers/tts_prediction.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+from Bio import SeqIO
+#import lib_classifier
+import pickle
+import sys
+from lib_classifier import load_instance
+
+if sys.argv[1] == '--help':
+	print('./tts_prediction [classifier] [genome]')
+	sys.exit()
+
+classifier = sys.argv[1]
+genome = sys.argv[2]
+classifier = load_instance(classifier)
+
+window_size = classifier.window_size
+print 'window_size = ', window_size
+print(window_size)
+for i in SeqIO.parse(genome, 'fasta'):
+	#print(i)
+	chromsome = i.name
+	seq = str(i.seq)
+	print 'chr = ', chromsome, '  from 1 to', len(seq) - window_size
+	for scanner in range(len(seq) - window_size):
+		subseq = seq[scanner : scanner + window_size]
+		re = classifier.predict([subseq])
+		#print(re[0])
+		if re[0] == '1': # in training set 1 means TTS and other means non-TTS
+			print scanner
+
+
+
+
