d497af71d11eb72e10b2f3e5354d71588d539878
Yanyu Liang <yanyul@andrew.cmu.edu>
2016-05-02 18:35:34 -0400

    fixed position unmatch

--- classifiers/tts_prediction.py
+++ classifiers/tts_prediction.py
@@ -70,13 +70,13 @@ for i in SeqIO.parse(genome, 'fasta'):
 			word = [str(chromsome), '\t', str(scanner + starter), '\t', str(scanner + starter), '\t'\
 					, 'predicted_tts', '\t', 'NA', '\t', '+']
 			eprint(''.join(word))
-			out.write(''.join(word))
+			out.write(''.join(word) + '\n')
 		re = classifier.predict([reverse_seq])
 		if re[0] == 1: # in training set 1 means TTS and other means non-TTS
 			word = [str(chromsome), '\t', str(scanner + starter), '\t', str(scanner + starter), '\t'\
 					, 'predicted_tts', '\t', 'NA', '\t', '-']
 			eprint(''.join(word))
-			out.write(''.join(word))
+			out.write(''.join(word) + '\n')
 
 
 
