dd230b02eeba0b2a0ce654d607bebc691529f76d
Yanyu Liang <yanyul@andrew.cmu.edu>
2016-05-05 23:51:36 -0400

    changed

--- classifiers/tts_prediction.py
+++ classifiers/tts_prediction.py
@@ -1,8 +1,38 @@
 from __future__ import print_function
 import sys
-
+import re
+# from pyfasta import Fasta
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+
+
+def report(subseq, new_char, reverse_seq, chromsome, scanner, starter, flag):
+	# print(subseq)
+	re = classifier.predict([subseq])
+	#print(re[0])
+	if flag == '':
+		reverse_seq = update_reverse_seq(reverse_seq, new_char)
+	if re[0] == 1: # in training set 1 means TTS and other means non-TTS
+		word = [str(chromsome), '\t', str(scanner + starter), '\t', str(scanner + starter), '\t'\
+				, 'predicted_tts', '\t', 'NA', '\t', '+']
+		eprint(''.join(word))
+		out.write(''.join(word) + '\n')
+	else:
+		eprint(str(chromsome) + '\t' + str(scanner + starter) + '\tskip\t+')
+	re = classifier.predict([reverse_seq])
+	if re[0] == 1: # in training set 1 means TTS and other means non-TTS
+		word = [str(chromsome), '\t', str(scanner + starter), '\t', str(scanner + starter), '\t'\
+				, 'predicted_tts', '\t', 'NA', '\t', '-']
+		eprint(''.join(word))
+		out.write(''.join(word) + '\n')
+	else:
+		eprint(str(chromsome) + '\t' + str(scanner + starter) + '\tskip\t-')
+	return reverse_seq
+
+def noreport(new_char, reverse_seq):
+	reverse_seq = update_reverse_seq(reverse_seq, new_char)
+	return reverse_seq
+
 
 from Bio import SeqIO
 #import lib_classifier
@@ -18,12 +48,13 @@ if len(sys.argv) == 1:
 	print('tts_prediction is for predicting the sites in a input genome, please use tts_prediction_train to train the model')
 	print
 	print('USAGE:')
-	print('python tts_prediction.py [classifier] [genome] [motif_path] [out]')
+	print('python tts_prediction.py [classifier] [genome] [motif_path] [jump_size] [out]')
 	print
 	print('for example:')
 	print('[classifier]: test_classifier')
 	print('[genome]: ./genome/test_genome.fa')
 	print('[motif_path]: ./motifs')
+	print('[jump_size]: 100')
 	print
 	sys.exit()
 
@@ -34,12 +65,13 @@ if sys.argv[1] == '--help':
 	print('tts_prediction is for predicting the sites in a input genome, please use tts_prediction_train to train the model')
 	print
 	print('USAGE:')
-	print('python tts_prediction.py [classifier] [genome] [motif_path] [out]')
+	print('python tts_prediction.py [classifier] [genome] [motif_path] [jump_size] [out]')
 	print
 	print('for example:')
 	print('[classifier]: test_classifier')
 	print('[genome]: ./genome/test_genome.fa')
 	print('[motif_path]: ./motifs')
+	print('[jump_size]: 100')
 	print
 	sys.exit()
 
@@ -47,42 +79,108 @@ classifier = sys.argv[1]
 genome = sys.argv[2]
 path = sys.argv[3]
 classifier = load_instance(classifier, path)
-out = sys.argv[4]
+jump_size = int(sys.argv[4])
+out = sys.argv[5]
 out = open(out, 'w')
 window_size = classifier.window_size
 eprint('window_size = ', window_size)
 eprint(window_size)
 starter = window_size / 2 + 1
-for i in SeqIO.parse(genome, 'fasta'):
+
+# f = Fasta(genome)
+f = open(genome, 'r')
+f = f.readlines()
+start_flag = 0
+seq = ''
+jump_counter = 0
+for l in f:
+	# f.pop()
+	# print(l)
+	l = re.sub('\n', '', l)
+	if l[0] == '>':
+		chromsome = l.split(':')
+		chromsome = chromsome[0][1:]
+		scanner = 0
+		start_flag = 1
+	else:
+		if start_flag == 1:
+			if len(seq + l) < window_size:
+				seq += l
+				continue
+			else:
+				d = window_size - len(seq)
+				seq += l[0:d]
+				subseq = seq
+				reverse_seq = reverse_complementary(subseq)
+				# print('scanner', scanner)
+				reverse_seq = report(subseq, '', reverse_seq, chromsome, scanner, starter, 'not')
+				start_flag = 0
+				tmp = l[d:]
+				chars = list(tmp)
+				for e in chars:
+					subseq = subseq[1:] + e
+					scanner += 1
+					# print('e = ',e)
+					jump_counter += 1
+					if jump_counter == jump_size:
+						reverse_seq = report(subseq, e, reverse_seq, chromsome, scanner, starter, '')
+						jump_counter = 0
+					else:
+						reverse_seq = noreport(e, reverse_seq)
+				# f.insert(0, tmp)
+				# print(f[0])
+				continue
+		elif start_flag == 0:
+			chars = list(l)
+			for e in chars:
+				subseq = subseq[1:] + e
+				scanner += 1
+				jump_counter += 1
+				if jump_counter == jump_size:
+					reverse_seq = report(subseq, e, reverse_seq, chromsome, scanner, starter, '')
+					jump_counter = 0
+				else:
+					reverse_seq = noreport(e, reverse_seq)
+				
+
+# print(f.keys())
+# for key in f.keys():
+	# chromsome = key
+	# seq = str(f[key])
+# for i in SeqIO.parse(genome, 'fasta'):
+
 	#print(i)
-	chromsome = i.name
-	seq = str(i.seq)
+	# chromsome = i.name
+	# seq = str(i.seq)
 	#print 'chr = ', chromsome, '  from 1 to', len(seq) - window_size
-	reverse_seq = seq[0 : window_size]
-	reverse_seq = reverse_complementary(reverse_seq)
-	for scanner in range(len(seq) - window_size):
-		subseq = seq[scanner : scanner + window_size]
-		if 'N' in subseq:
-			continue
-		new_char = seq[scanner + window_size - 1]
-		re = classifier.predict([subseq])
-		#print(re[0])
-		reverse_seq = update_reverse_seq(reverse_seq, new_char)
-		if re[0] == 1: # in training set 1 means TTS and other means non-TTS
-			word = [str(chromsome), '\t', str(scanner + starter), '\t', str(scanner + starter), '\t'\
-					, 'predicted_tts', '\t', 'NA', '\t', '+']
-			eprint(''.join(word))
-			out.write(''.join(word) + '\n')
-		else:
-			eprint(str(chromsome) + '\t' + str(scanner + starter) + '\tskip\t+')
-		re = classifier.predict([reverse_seq])
-		if re[0] == 1: # in training set 1 means TTS and other means non-TTS
-			word = [str(chromsome), '\t', str(scanner + starter), '\t', str(scanner + starter), '\t'\
-					, 'predicted_tts', '\t', 'NA', '\t', '-']
-			eprint(''.join(word))
-			out.write(''.join(word) + '\n')
-		else:
-			eprint(str(chromsome) + '\t' + str(scanner + starter) + '\tskip\t-')
+	# reverse_seq = seq[0 : window_size]
+	# reverse_seq = reverse_complementary(reverse_seq)
+	# for scanner in range(len(seq) - window_size):
+	# 	# subseq = seq[scanner : scanner + window_size]
+	# 	print('1\t')
+	# 	subseq = str(f[key][scanner : scanner + window_size])
+	# 	print(subseq)
+	# 	if 'N' in subseq:
+	# 		continue
+	# 	new_char = str(f[key][scanner + window_size - 1])
+	# 	re = classifier.predict([subseq])
+	# 	#print(re[0])
+	# 	reverse_seq = update_reverse_seq(reverse_seq, new_char)
+	# 	if re[0] == 1: # in training set 1 means TTS and other means non-TTS
+	# 		word = [str(chromsome), '\t', str(scanner + starter), '\t', str(scanner + starter), '\t'\
+	# 				, 'predicted_tts', '\t', 'NA', '\t', '+']
+	# 		eprint(''.join(word))
+	# 		out.write(''.join(word) + '\n')
+	# 	else:
+	# 		eprint(str(chromsome) + '\t' + str(scanner + starter) + '\tskip\t+')
+	# 	re = classifier.predict([reverse_seq])
+	# 	if re[0] == 1: # in training set 1 means TTS and other means non-TTS
+	# 		word = [str(chromsome), '\t', str(scanner + starter), '\t', str(scanner + starter), '\t'\
+	# 				, 'predicted_tts', '\t', 'NA', '\t', '-']
+	# 		eprint(''.join(word))
+	# 		out.write(''.join(word) + '\n')
+	# 	else:
+	# 		eprint(str(chromsome) + '\t' + str(scanner + starter) + '\tskip\t-')
 
 
 
--- classifiers/tts_prediction_train.py
+++ classifiers/tts_prediction_train.py
@@ -66,49 +66,6 @@ feature_gen.add_function('motif_score', ['motif1.motif', '-b']) # param is [moti
 feature_gen.add_function('motif_score', ['motif2.motif', '-b']) 
 feature_gen.add_function('motif_score', ['motif3.motif','-b']) 
 feature_gen.add_function('motif_score', ['motif4.motif', '-b']) 
-feature_gen.add_function('motif_score', ['dreme 1st AYAYA.txt', '-b'])
-feature_gen.add_function('motif_score', ['dreme 2nd AAWWAA.txt', '-b'])
-feature_gen.add_function('motif_score', ['dreme 3rd KTTMA.txt', '-b'])
-feature_gen.add_function('motif_score', ['dreme 4th AGCAAY.txt', '-b'])
-feature_gen.add_function('motif_score', ['dreme 5th GTAATW.txt', '-b'])
-feature_gen.add_function('motif_score', ['dreme 6th CATD.txt', '-b'])
-feature_gen.add_function('motif_score', ['dreme 7th CAAWCA.txt', '-b'])
-feature_gen.add_function('motif_score', ['dreme 8th GCAGY.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 5 1st.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 5 2nd.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 5 3rd.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 5 4th.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 5 5th.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 6 1st.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 6 2nd.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 6 3rd.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 6 4th.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 6 5th.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 7 1st.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 7 2nd.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 7 3rd.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 7 4th.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 7 5th.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 8 1st.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 8 2nd.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 8 3rd.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 8 4th.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 8 5th.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 9 1st.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 9 2nd.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 9 3rd.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 9 4th.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 9 5th.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 10 1st.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 10 2nd.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 10 3rd.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 10 4th.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w 10 5th.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w auto 1st.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w auto 2nd.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w auto 3rd.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w auto 4th.txt', '-b'])
-feature_gen.add_function('motif_score', ['meme -w auto 5th.txt', '-b'])
 
 # RNA secondary structure
 feature_gen.add_function('rna_struct', [14, '']) # usage of RNA secondary structure feature. NUM means the length of the sequence you want to analyze the structure. more details are in lib_classifier
@@ -134,11 +91,11 @@ my_classifier = lib_classifier.My_Classifier(feature_gen, 'tree', [100,1,'rbf_li
 # Training
 (raw_data, labels) = data_read(my_in) 
 my_classifier.train(raw_data, labels) # load training file
-# my_classifier.feature_to_csv(raw_data, labels, 'train.csv') # output design matrix of training file
+my_classifier.feature_to_csv(raw_data, labels, 'train.csv') # output design matrix of training file
 
 # Testing
 (raw_data_test, labels_test) = data_read(my_test) # read test set from file
-# my_classifier.feature_to_csv(raw_data_test, labels_test, 'test.csv') # output design matrix of testing file
+my_classifier.feature_to_csv(raw_data_test, labels_test, 'test.csv') # output design matrix of testing file
 re = my_classifier.predict(raw_data_test) # predict
 
 # Report Training Accuracy
